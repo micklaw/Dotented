f4864e7543a63e02e949037303faa3dff10a4fca
Michael Law <michael@mlwdltd.onmicrosoft.com>
2020-11-04 21:35:38 +0000

    PAcking nuget

---
+++ Example/Models/Components/Skills.cs
@@ -0,0 +1,13 @@
+using Dotented.Interfaces;
+
+namespace MySite.Components
+{
+    public class Skills : DotentedContent
+    {
+        public string Title { get; set; }
+
+        public string Icon { get; set; }
+
+        public string Details { get; set; }
+    }
+}
